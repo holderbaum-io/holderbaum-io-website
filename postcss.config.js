ce48c44472a73fc63cc64786a3f60b3cd8e62d4e
Jakob Holderbaum <jakob@holderbaum.io>
2019-09-12 07:38:38 +0200

    Install custom property plugin for nextcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: [
     require('postcss-import')({ path: 'source/stylesheets' }),
-    require('postcss-preset-env')({ stage: 0 })
+    require('postcss-preset-env')({ stage: 4 }),
+    require('postcss-custom-properties')({ preserve: false })
   ]
 }

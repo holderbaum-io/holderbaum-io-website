67a0e888cfa256b7c540e12f5aa2f2c8daa1c92c
Jakob Holderbaum <jakob@holderbaum.io>
2019-09-12 07:40:58 +0200

    Install calc plugin for postcss

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     require('postcss-import')({ path: 'source/stylesheets' }),
     require('postcss-preset-env')({ stage: 4 }),
-    require('postcss-custom-properties')({ preserve: false })
+    require('postcss-custom-properties')({ preserve: false }),
+    require('postcss-calc')({ preserve: false })
   ]
 }
